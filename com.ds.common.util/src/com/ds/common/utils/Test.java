d44010bf0f41dd18e49ed8ee5776670ecf2a4f50
zyl <zyl@zyl.com>
2018-12-26 16:01:56 +0800

    新建工程，增加测试类

---
+++ com.ds.common.util/src/com/ds/common/utils/Test.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: Test
+ * Author:   Adidas
+ * Date:     2018/12/26 15:37
+ * Description: 本工程为常用工具总结工程，并学习使用git，本类为测试类
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.ds.common.utils;
+
+/**
+ * 本工程为常用工具总结工程，并学习使用git，本类为测试类
+ *
+ * @author Adidas
+ * @create 2018/12/26
+ * @since 1.0.0
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+
+}
